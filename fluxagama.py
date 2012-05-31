e27532fb3b0fbca65791def032fd28776d12b975
Nicolai Czempin <n.czempin@gmx.de>
2012-05-31 16:50:41 +0200

    cleanup: whitespace, moved all parts of the main program to a single place

--- fluxagama.py
+++ fluxagama.py
@@ -9,18 +9,6 @@ def load_image(filename):
     except pygame.error:
         raise SystemExit, 'Could not load image "%s" %s' % (filename, pygame.get_error())
     return surface
-
- 
-pygame.init()
-if pygame.mixer and not pygame.mixer.get_init():
-        print 'Warning, no sound'
-        pygame.mixer = None
-screenX = 800
-screenY = 600
-window = pygame.display.set_mode((screenX, screenY)) 
-pygame.display.set_caption('LOLOLOL') 
-screen = pygame.display.get_surface()
-
 class dummysound:
     def play(self): pass
     
@@ -35,39 +23,9 @@ def load_sound(filename):
         print 'Warning, unable to load,', filename
     return dummysound()
 
-shoot_sound = load_sound('Schuss.wav')
+ 
 
-ship_surface = load_image("Flieger.png")
-shot_surface = load_image("Schuss.png")
-enemy0_surface = load_image("UFO.png")
-
-shipX, shipY = ship_surface.get_size()
-shotX, shotY = shot_surface.get_size()
-enemy0X, enemy0Y = enemy0_surface.get_size()
-
-ship_coorX = (screenX - shipX) / 2
-ship_coorY = screenY - shipY
-
-shot_coorX = 0
-shot_coorY = 0
-
-enemy0_coorX = 300
-enemy0_coorY = 20
-
-shot_exists = False
-enemy0_exists = True
-
-
-
-
-#2. Schuss erscheint nur wenn aktiv  done
-#3. aktiv wird durch taste umgeschaltet done
-#4. aktiver schuss bewegt sich  done
-#5. schuss verschwindet am rand   done
-#6. schuss wirdan der richtigen stelle abgefeuert done
-
-
-def input(events):
+def handle_input(events):
     global ship_coorX
     global ship_coorY  
     global shot_exists
@@ -97,8 +55,38 @@ def input(events):
                 shoot_sound.play()
                 shot_exists = True
                 shot_coorX, shot_coorY = ship_coorX + (shipX - shotX) / 2, ship_coorY - shotY
-            
-      
+pygame.init()
+if pygame.mixer and not pygame.mixer.get_init():
+    print 'Warning, no sound'
+    pygame.mixer = None
+screenX = 800
+screenY = 600
+window = pygame.display.set_mode((screenX, screenY)) 
+pygame.display.set_caption('LOLOLOL') 
+screen = pygame.display.get_surface()
+
+
+shoot_sound = load_sound('Schuss.wav')
+
+ship_surface = load_image("Flieger.png")
+shot_surface = load_image("Schuss.png")
+enemy0_surface = load_image("UFO.png")
+
+shipX, shipY = ship_surface.get_size()
+shotX, shotY = shot_surface.get_size()
+enemy0X, enemy0Y = enemy0_surface.get_size()
+
+ship_coorX = (screenX - shipX) / 2
+ship_coorY = screenY - shipY
+
+shot_coorX = 0
+shot_coorY = 0
+
+enemy0_coorX = 300
+enemy0_coorY = 20
+
+shot_exists = False
+enemy0_exists = True
 done = False
 try:
     while not done:
@@ -116,23 +104,19 @@ try:
             else:
                 enemy0_exists = False
                 shot_exists = False
-
         if shot_exists:
             screen.blit (shot_surface, (shot_coorX, shot_coorY))
             shot_coorY -= 1
             if shot_coorY <= 0:
                 shot_exists = False
 
-
-
         if enemy0_exists == True:
             screen.blit (enemy0_surface, (enemy0_coorX, enemy0_coorY))
       
         screen.blit (ship_surface, (ship_coorX, ship_coorY))
-
     
         pygame.display.flip()
-        done = input(pygame.event.get())
+        done = handle_input(pygame.event.get())
 except:
     print 'Warning, Error,', file
 pygame.quit()
