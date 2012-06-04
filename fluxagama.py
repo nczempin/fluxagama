9df8e60db321099aa3b332eaf6a8db69661a1f02
Nicolai Czempin <n.czempin@gmx.de>
2012-06-04 22:02:32 +0200

    enemies get removed from the list when they die

--- fluxagama.py
+++ fluxagama.py
@@ -80,76 +80,77 @@ score1textpos.centery = 36
 
 
 # Main game loop
-try:
-    while not done:
-        clock.tick()
-        ticks += 1
-        if (ticks % 1000)==0:
-            fps = clock.get_fps()
-            print fps
-        # draw black background
-        screen.fill((0, 0, 0))
-        #draw text
-        screen.blit(score1titletext, score1titletextpos)
-        screen.blit(score1text, score1textpos)
-       
-        for i in range(len(enemies)):
-            screen.blit (enemy0_surface, (enemies[i][0], enemies[i][1]))
-            # collision detection shot <-> enemy
-            if enemies[i][1] + enemy0Y < shot_coorY:
-                pass
-            elif enemies[i][1] > shot_coorY + shotY:
-                pass
-            elif enemies[i][0] > shot_coorX + shotX:
-                pass
-            elif enemies[i][0] + enemy0X < shot_coorX:
-                pass
-            else:
-                # Collision!
-                enemy0_exists = False
-                shot_exists = False
-                enemy_explosion_sound.play()
-                # TODO: enemy explosion
-        if shot_exists:
-            # draw and move shot
-            screen.blit (shot_surface, (shot_coorX, shot_coorY))
-            shot_coorY -= 1
-            if shot_coorY <= BORDER_UPPER:
-                shot_exists = False
+while not done:
+    clock.tick()
+    ticks += 1
+    if (ticks % 1000)==0:
+        fps = clock.get_fps()
+        print fps
+    # draw black background
+    screen.fill((0, 0, 0))
+    #draw text
+    screen.blit(score1titletext, score1titletextpos)
+    screen.blit(score1text, score1textpos)
+    dying_enemies = [] # empty list that gets filled when enemies get shot
+    for i in range(len(enemies)):
+        screen.blit (enemy0_surface, (enemies[i][0], enemies[i][1]))
+        # collision detection shot <-> enemy
+        if enemies[i][1] + enemy0Y < shot_coorY:
+            pass
+        elif enemies[i][1] > shot_coorY + shotY:
+            pass
+        elif enemies[i][0] > shot_coorX + shotX:
+            pass
+        elif enemies[i][0] + enemy0X < shot_coorX:
+            pass
+        else:
+            # Collision!
+            dying_enemies.append(i)
+            shot_exists = False
+            enemy_explosion_sound.play()
+            # TODO: enemy explosion
+    delta = 0
+    for i in range(len(dying_enemies)):
+        del enemies[dying_enemies[i+delta]]
+        delta += 1 # each time we remove one, the index of all the others must be reduced. This assumes that the list of dying enemies is sorted
+    if shot_exists:
+        # draw and move shot
+        screen.blit (shot_surface, (shot_coorX, shot_coorY))
+        shot_coorY -= 1
+        if shot_coorY <= BORDER_UPPER:
+            shot_exists = False
 
-        if enemy0_exists:
-            # draw enemy TODO move enemy
-            pass#screen.blit (enemy0_surface, (enemies[0][0], enemies[0][1]))
-      
-        # draw player ship
-        screen.blit (ship_surface, (ship_coorX, ship_coorY))
+    if enemy0_exists:
+        # draw enemy TODO move enemy
+        pass#screen.blit (enemy0_surface, (enemies[0][0], enemies[0][1]))
+  
+    # draw player ship
+    screen.blit (ship_surface, (ship_coorX, ship_coorY))
 
-        # swap baCK AND FRONT BUFFERS
-        pygame.display.flip()
-        # read keyboard and move player ship
-        events = pygame.event.get()
-        keystate = pygame.key.get_pressed()
-    
-        if keystate[K_a] == 1:
-            if ship_coorX > BORDER_LEFT:
-                ship_coorX -= 1
-        if keystate[K_d] == 1:
-            if ship_coorX + shipX < BORDER_RIGHT:
-                ship_coorX += 1
-        if keystate[K_SPACE] == 1:
-            if not shot_exists:
-                shoot_sound.play()
-                shot_exists = True
-                shot_coorX, shot_coorY = ship_coorX + (shipX - shotX) / 2, ship_coorY - shotY
-    
-        for event in events: 
-            if event.type == QUIT:# or
-            #if event.key == K_ESCAPE: 
-                done= True
-    #        elif event.type == KEYDOWN:
-    #            print event.scancode
-    #            if event.scancode == 57 and not shot_exists:
+    # swap baCK AND FRONT BUFFERS
+    pygame.display.flip()
+    # read keyboard and move player ship
+    events = pygame.event.get()
+    keystate = pygame.key.get_pressed()
 
-except:
-    print 'Warning, Error'
+    if keystate[K_a] == 1:
+        if ship_coorX > BORDER_LEFT:
+            ship_coorX -= 1
+    if keystate[K_d] == 1:
+        if ship_coorX + shipX < BORDER_RIGHT:
+            ship_coorX += 1
+    if keystate[K_SPACE] == 1:
+        if not shot_exists:
+            shoot_sound.play()
+            shot_exists = True
+            shot_coorX, shot_coorY = ship_coorX + (shipX - shotX) / 2, ship_coorY - shotY
+
+    for event in events: 
+        if event.type == QUIT:# or
+        #if event.key == K_ESCAPE: 
+            done= True
+#        elif event.type == KEYDOWN:
+#            print event.scancode
+#            if event.scancode == 57 and not shot_exists:
+
 pygame.quit()
