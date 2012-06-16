2fee95f9ec859114977ff5df1d565aa742302bce
Pavel Czempin <valep.pave@gmx.de>
2012-06-16 21:33:10 +0200

    Added sprite for explosion

--- src/fluxagama/explosion.py
+++ src/fluxagama/explosion.py
@@ -1,20 +1,25 @@
 import pygame
 import graphics
-explosion_image = graphics.load_image("explosion00.png")
+#explosion_image = graphics.load_image("explosion00.png")
+
 e = None
 def create(position):
     global e
     e = Explosion(position)
     #explosions.append(e)
 def draw(surface):
-    global e, explosion_image
+    global e
     if e != None:
-        if e.ttl >0:
-            surface.blit(explosion_image,e.position)
-            e.ttl -= 1
+        if e.ttl > 0:
+            e.draw(surface)
         else:
             e = None
 class Explosion(pygame.sprite.Sprite):
     def __init__(self,position):
         self.position = position
+        pygame.sprite.Sprite.__init__(self)
+        self.image = graphics.load_image("explosion00.png")
         self.ttl = 60 #number of ticks that this will "live"
+    def draw(self, surface):
+        surface.blit(self.image,self.position)
+        self.ttl -= 1
